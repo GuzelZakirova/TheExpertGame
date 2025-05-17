2651d46f01a1a0f8a8af3986195507f1b80ff22a
Guzel Zakirova <guz.zakirova@innopolis.university>
2025-05-17 14:42:27 +0300

    Add main execution block to the program

--- expert.py
+++ expert.py
@@ -6,15 +6,29 @@ def read_from_file():
         for line in file:
             line = line.rstrip('\n')  # Удаляет символ "перевод строки"
             attack, definition = line.split('/')  # Символ '/' разделяет строку, в attack попадают названия кибер-атак, а в definition - их определения
-            cyber_attack[attack] = definition  # attack является ключом, а definition - значением
+            cyber_attacks[attack] = definition  # attack является ключом, а definition - значением
 
 # Функция для записи данных в текстовый файл
 def write_to_file(attack_type, definition_text):
     with open('cyber_attack_data.txt', ' a', encoding='utf-8') as file:
-        file.write('\n', attack_type, '/', definition_text)  # Записывает в файл тип атаки (attack_type) и ее определение (definition_text)
+        file.write('\n', attack_type, '/', definition_text)
 
 print('The Expert - Cyber Attacks')
 root = Tk()
 root.withdraw()
 
+# Основной блок программы
 cyber_attacks = {}  # Создаем пустой словарь для хранения кибер-атак и их определений
+
+read_from_file()
+
+while True:
+    query_string = simpledialog.askstring('The Expert', 'Введите тип кибер-атаки:')
+
+    if query_string in cyber_attacks:
+        answer = cyber_attacks[query_string]
+        messagebox.showinfo('Answer', query_string + ': ' + answer)
+    else:
+        answer = simpledialog.askstring('Расскажи мне', 'Введите определение кибер-атаки:')
+        cyber_attacks[query_string] = answer
+        write_to_file(query_string, answer)
